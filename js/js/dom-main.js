fe477b39cf86b1bf3c33c3bbb7d42a06c445ef3c
pharheen <farheensayyed2006@gmail.com>
2021-01-21 15:15:09 +0530

    document object module

---
+++ js/js/dom-main.js
@@ -0,0 +1,8 @@
+var element;
+
+//element = document.all;
+//element = document.links[0];//for links show target..
+//element = document.getElementById("content").innerHTML;
+element = document.getElementById("header").innerHTML;
+console.log(element);
+
